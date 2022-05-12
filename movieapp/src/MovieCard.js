34216437b5ae7b2444d382ab1d1a659c5cdfbefe
andrebroomes <tru3wreckz@gmail.com>
2022-05-12 13:29:57 -0400

    done

--- movieapp/src/MovieCard.js
+++ movieapp/src/MovieCard.js
@@ -2,8 +2,9 @@ function MovieCard({image, year, title}){
 
     return(
         <div className="card" >
-          <img src={image}></img>  
+          <img src={image} className="img"></img>
           <div className="container">
+          
               <h1>{title}</h1>
               <h1>{year}</h1>
           </div>
